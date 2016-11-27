8cbedfa76f60b85322a7028846fa3659a10ab280
jasontgalvin <jtgalvin@comcast.net>
2016-11-27 18:51:39 -0400

    Added a riddle to entrance room.
    A "note" was added to a the wall near the west door. This should catch
    the user's eye and they theoretically investigate it. UI text will then
    give them a riddle to find the key for that room.

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Note.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoteObject : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void readNote()
+    {
+        GameObject refg = GameObject.Find("TextController");
+        TextController t = refg.GetComponent<TextController>();
+
+        UnityStandardAssets.Characters.FirstPerson.FirstPersonController.key_1 = true;
+
+
+        t.textUpdate("What you seek is hidden in an object.\nLook closely at an object hard and sturdy. \n press enter");
+
+
+    }
+}
