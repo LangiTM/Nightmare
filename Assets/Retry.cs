b5e0709d478284ab776fe0d22db220bd484d4ee6
LangiTM <LangilleTM@Gmail.com>
2016-12-06 22:38:25 -0400

    Added checkpoints
    Only checkpoint to mainfloor currently. Will add checkpoint to backfloor
    when added.

---
+++ Assets/Retry.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Retry : MonoBehaviour {
+    public string level;
+
+	// Use this for initialization
+	void Start () {
+        level = FirstPersonController.currLevel;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnMouseDown()
+    {
+        //level = GameObject.Find("FPSController").GetComponent<FirstPersonController>().currLevel;
+        Debug.Log(level);
+        Application.LoadLevel(level);
+    }
+}
